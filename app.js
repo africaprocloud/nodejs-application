2e965216d76d25fea04977f923865323b373b3ec
africaprocloud <africaprocloudinstitute.apci@gmail.com>
2024-10-07 13:07:05 +0100

    Update app.js

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ res.send("<h2>Welcome to Node JS express app</h2>"+appEnv.url+appEnv.port+port+p
 }).listen(9009);
 console.log(__dirname+"/images/africaprocloudlogo.png");
 */
-app.get('/SolaviseTech', function(request, response) {
+app.get('/africaprocloud', function(request, response) {
     //response.send("<h2><center>Welcome to Node JS app developed by africaprocloud</h2>");
     response.write("<h2><center><u>Node JS  Application </u></center></h2>");
 	
